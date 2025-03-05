b0167e504ca0d0aa0809679b0ed1a4b1205bd2e5
Arvo <cantarvo@gmail.com>
2025-03-05 17:40:32 +0100

    Throw error

--- src/main/java/org/kaas/Main.java
+++ src/main/java/org/kaas/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Extra feature");
+
+        /// Oh no an error
+        throw new RuntimeException("Oh no!");
     }
 }
